01731138fee605457ac58226e3307d3951fdf617
&#8238 <91245520+qqqqqqqq1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq@users.noreply.github.com>
2022-02-10 09:55:02 +0300

    Update and rename izda.py to zda.py

--- izda.py
+++
@@ -1,8 +0,0 @@
-def f(s):
-  return s
-
-n=int(input())
-a=float(input())
-b=float(input())
-x=float(input())
-l=list
---
+++ zda.py
@@ -0,0 +1,29 @@
+def f(s):
+  return s
+
+def x(s):
+  return a+(b-a)/n*s
+
+def c(s,lam):
+  for j in range(0,k):
+    lam=lam*(s-x(j))
+  for j in range(k,n+1):
+    lam=lam*(s-x(j))
+  return lam
+  
+n=int(input())
+a=float(input())
+b=float(input())
+xx=float(input())
+l=[]
+for i in range(0,n+1):
+  h=1.0
+  for j in range(0,k):
+    h=h/(x(i)-x(j))
+  for j in range(k,n+1):
+    h=h/(x(i)-x(j)) 
+    l.append(h)
+L=0.0
+for i in range(0,n+1):
+  L = L + c (xx, l[i]) * f( x(i) )
+print(L)
